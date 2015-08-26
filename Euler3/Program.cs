44718ad0d294c562e8555f1121c9602be7d572c5
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2015-08-26 06:59:18 +0200

    Initial valid solution

---
+++ Euler3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Euler3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong n;
+            ulong.TryParse(args[0], out n);
+
+            uint high = 2;
+
+            while (n % 2 == 0)
+                n = n / 2;
+
+            uint f = 3;
+
+            while (n > 1)
+            {
+                if( n % f == 0)
+                {
+                    n = n / f;
+                    high = f;
+                } else
+                {
+                    f += 2;
+                }
+            }
+
+            Console.WriteLine(high);
+            Console.ReadLine();
+        }
+    }
+}
